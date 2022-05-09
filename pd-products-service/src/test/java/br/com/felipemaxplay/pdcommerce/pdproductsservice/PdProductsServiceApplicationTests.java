92596a1e5d9dfdc72794d95584d7460501437169
felipemaxplay <felipemaxplay@gmail.com>
2022-05-09 12:12:22 -0300

    init pd-products-sercive

---
+++ pd-products-service/src/test/java/br/com/felipemaxplay/pdcommerce/pdproductsservice/PdProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipemaxplay.pdcommerce.pdproductsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdProductsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
