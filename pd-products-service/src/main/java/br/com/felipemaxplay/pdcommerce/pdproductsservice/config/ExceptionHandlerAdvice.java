b64ba4dbfadcf1e079f29a2c97fd1cb9cc68adb2
felipemaxplay <felipemaxplay@gmail.com>
2022-05-16 10:45:10 -0300

    exception hanlder and response error created

--- pd-products-service/src/main/java/br/com/felipemaxplay/pdcommerce/pdproductsservice/config/ExceptionHandlerAdvice.java
+++ pd-products-service/src/main/java/br/com/felipemaxplay/pdcommerce/pdproductsservice/config/ExceptionHandlerAdvice.java
@@ -1,4 +1,22 @@
 package br.com.felipemaxplay.pdcommerce.pdproductsservice.config;
 
+import br.com.felipemaxplay.pdcommerce.pdproductsservice.http.data.response.PdError;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.persistence.NoResultException;
+import java.util.Date;
+
+@ControllerAdvice
 public class ExceptionHandlerAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(NoResultException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public PdError notFoundException(NoResultException e) {
+        return new PdError(new Date(), "404", e.getMessage());
+    }
 }
--- pd-products-service/src/main/java/br/com/felipemaxplay/pdcommerce/pdproductsservice/http/data/response/PdError.java
+++ pd-products-service/src/main/java/br/com/felipemaxplay/pdcommerce/pdproductsservice/http/data/response/PdError.java
@@ -1,4 +1,39 @@
 package br.com.felipemaxplay.pdcommerce.pdproductsservice.http.data.response;
 
+import org.springframework.lang.NonNull;
+
+import java.util.Date;
+import java.util.Objects;
+
 public class PdError {
+    private final Date timestamp;
+    private final String code;
+    private final String message;
+
+    public PdError(@NonNull Date timestamp, @NonNull String code, @NonNull String message) {
+        this.timestamp = Objects.requireNonNull(timestamp);
+        this.code = Objects.requireNonNull(code);
+        this.message = Objects.requireNonNull(message);
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "PdError{" +
+                "timestamp=" + timestamp +
+                ", code='" + code + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
