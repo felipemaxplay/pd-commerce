a279e1bbe6caa6f85a0ed1c54f58167b339ed3d1
felipemaxplay <felipemaxplay@gmail.com>
2022-05-26 21:36:01 -0300

    change generate type indentity to auto in models

--- PdOrdersService/src/main/java/br/com/felipemaxplay/pdcommerce/pdorders/PdOrdersService/model/Order.java
+++ PdOrdersService/src/main/java/br/com/felipemaxplay/pdcommerce/pdorders/PdOrdersService/model/Order.java
@@ -15,7 +15,7 @@ import java.util.Set;
 @Table(name = "pd_orders")
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
 
--- PdOrdersService/src/main/java/br/com/felipemaxplay/pdcommerce/pdorders/PdOrdersService/model/OrderProduct.java
+++ PdOrdersService/src/main/java/br/com/felipemaxplay/pdcommerce/pdorders/PdOrdersService/model/OrderProduct.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 @Table(name = "pd_order_product")
 public class OrderProduct {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
 
