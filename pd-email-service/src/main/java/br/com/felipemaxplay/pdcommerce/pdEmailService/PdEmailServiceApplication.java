748f32dc3c670ce297c17d327a9cbbc025e943bb
felipemaxplay <felipemaxplay@gmail.com>
2022-06-01 12:01:19 -0300

    init pd-email-service

---
+++ pd-email-service/src/main/java/br/com/felipemaxplay/pdcommerce/pdEmailService/PdEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipemaxplay.pdcommerce.pdEmailService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdEmailServiceApplication.class, args);
+	}
+
+}
---
+++ pd-email-service/src/test/java/br/com/felipemaxplay/pdcommerce/pdEmailService/PdEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipemaxplay.pdcommerce.pdEmailService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdEmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
