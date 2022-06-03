d481541caef97da123e8c71a60293aa020719075
felipemaxplay <felipemaxplay@gmail.com>
2022-06-03 10:45:49 -0300

    email-service enable eureka client, and adjust props starttls-enable

--- pd-email-service/src/main/java/br/com/felipemaxplay/pdcommerce/pdEmailService/PdEmailServiceApplication.java
+++ pd-email-service/src/main/java/br/com/felipemaxplay/pdcommerce/pdEmailService/PdEmailServiceApplication.java
@@ -2,8 +2,10 @@ package br.com.felipemaxplay.pdcommerce.pdEmailService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PdEmailServiceApplication {
 
 	public static void main(String[] args) {
