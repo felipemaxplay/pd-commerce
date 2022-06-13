7ae849fb1604f0e62e0f005e7b9853536024651c
felipemaxplay <felipemaxplay@gmail.com>
2022-06-13 11:53:15 -0300

    pd-gateway init security

---
+++ pd-gateway/src/main/java/br/com/felipemaxplay/pdcommerce/pdGateway/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package br.com.felipemaxplay.pdcommerce.pdGateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        http.csrf().disable();
+        return http.build();
+    }
+}
