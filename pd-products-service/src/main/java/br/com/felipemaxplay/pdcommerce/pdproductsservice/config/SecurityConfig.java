f89285225065c5b97e1f408048ce2684793de213
felipemaxplay <felipemaxplay@gmail.com>
2022-06-15 10:36:45 -0300

    deprecated extends WebSecurityConfigurerAdapter

--- pd-products-service/src/main/java/br/com/felipemaxplay/pdcommerce/pdproductsservice/config/SecurityConfig.java
+++ pd-products-service/src/main/java/br/com/felipemaxplay/pdcommerce/pdproductsservice/config/SecurityConfig.java
@@ -1,14 +1,14 @@
 package br.com.felipemaxplay.pdcommerce.pdproductsservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+public class SecurityConfig {
     private static final String[] WHITELIST = {
             "/documentation",
             "/api-documentation",
@@ -17,8 +17,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/swagger-ui/**"
     };
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(WHITELIST)
                 .permitAll()
@@ -27,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .oauth2ResourceServer()
                 .jwt();
+        return http.build();
     }
 
 }
